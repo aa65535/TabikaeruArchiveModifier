ce5dfac695e641a90df480c38455004427ed0749
Jian Chang <aa65535@live.com>
2018-01-24 17:58:18 +0800

    minor fix

--- app/src/main/java/com/aa65535/tabikaeruarchivemodifier/MainActivity.java
+++ app/src/main/java/com/aa65535/tabikaeruarchivemodifier/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -33,16 +35,24 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         //noinspection ConstantConditions
         archive = new File(getExternalCacheDir().getParentFile().getParentFile(),
                 "jp.co.hit_point.tabikaeru/files/GameData.sav");
-        verifyStoragePermissions(this);
+        if (archive.exists()) {
+            verifyStoragePermissions(this);
+        } else {
+            showToast(R.string.archive_not_found);
+        } 
     }
 
     private void initView() {
-        clover = findViewById(R.id.et_clover);
-        tickets = findViewById(R.id.et_tickets);
-        findViewById(R.id.save_clover).setOnClickListener(this);
-        findViewById(R.id.save_tickets).setOnClickListener(this);
-        clover.setText(getString(R.string.number, readInt(archive, 0xc70)));
-        tickets.setText(getString(R.string.number, readInt(archive, 0xc74)));
+        if (archive.canWrite()) {
+            clover = findViewById(R.id.et_clover);
+            tickets = findViewById(R.id.et_tickets);
+            findViewById(R.id.save_clover).setOnClickListener(this);
+            findViewById(R.id.save_tickets).setOnClickListener(this);
+            clover.setText(getString(R.string.number, readInt(archive, 0xc70)));
+            tickets.setText(getString(R.string.number, readInt(archive, 0xc74)));
+        } else {
+            showToast(R.string.archive_permission_denied);
+        }
     }
 
     public void verifyStoragePermissions(Activity activity) {
@@ -55,26 +65,22 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                         "android.permission.WRITE_EXTERNAL_STORAGE"
                 }, REQUEST_CODE);
             } else {
-                if (archive.canWrite()) {
-                    initView();
-                } else {
-                    Toast.makeText(this, R.string.archive_open_failed_msg, Toast.LENGTH_SHORT).show();
-                }
+                initView();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private void showToast(@StringRes int resId) {
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE) {
-            if (archive.canWrite()) {
-                initView();
-            } else {
-                Toast.makeText(this, R.string.archive_open_failed_msg, Toast.LENGTH_SHORT).show();
-            }
+            initView();
         }
     }
 
@@ -82,19 +88,20 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     public void onClick(View v) {
         try {
             String s;
+            boolean ret = false;
             switch (v.getId()) {
                 case R.id.save_clover:
                     s = clover.getText().toString();
-                    writeInt(archive, 0xc70, Integer.parseInt(s));
+                    ret = writeInt(archive, 0xc70, Integer.parseInt(s));
                     break;
                 case R.id.save_tickets:
                     s = tickets.getText().toString();
-                    writeInt(archive, 0xc74, Integer.parseInt(s));
+                    ret = writeInt(archive, 0xc74, Integer.parseInt(s));
                     break;
             }
-            Toast.makeText(this, R.string.success_msg, Toast.LENGTH_SHORT).show();
+            showToast(ret ? R.string.success_msg : R.string.failure_msg);
         } catch (NumberFormatException e) {
-            Toast.makeText(this, R.string.number_err_msg, Toast.LENGTH_SHORT).show();
+            showToast(R.string.number_err_msg);
         }
     }
 
@@ -120,6 +127,17 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         return bb.getInt();
     }
 
+    private static void closeQuietly(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (RuntimeException rethrown) {
+                throw rethrown;
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
     private static int readInt(File archive, int offset) {
         RandomAccessFile r = null;
         try {
@@ -129,33 +147,23 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try {
-                if (r != null) {
-                    r.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            closeQuietly(r);
         }
         return -1;
     }
 
-    private static void writeInt(File archive, int offset, int value) {
+    private static boolean writeInt(File archive, int offset, int value) {
         RandomAccessFile r = null;
         try {
-            r = new RandomAccessFile(archive, "rw");
+            r = new RandomAccessFile(archive, "rwd");
             r.seek(offset);
             r.writeInt(bigToLittle(value));
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try {
-                if (r != null) {
-                    r.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            closeQuietly(r);
         }
+        return false;
     }
 }
